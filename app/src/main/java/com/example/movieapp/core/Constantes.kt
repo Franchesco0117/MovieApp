e857c4a36d95bfac4fbf1dcc548df891ad794fa4
franciscocastromurillo18@gmail.com <franciscocastromurillo18@gmail.com>
2024-02-15 19:35:00 -0700

    Obtenemos informacion del End point

---
+++ app/src/main/java/com/example/movieapp/core/Constantes.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.core
+
+object Constantes {
+    const val API_Key = "177776d71e712e14c6ced2b6b72989af"
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    const val BASE_URL_IMAGEN = "https://image.tmdb.org/t/p/w500"
+
+    const val IMAGEN_ANCHO = 330
+    const val IMAGEN_ALTO = 330
+
+    const val MAX_CALIFICACION = 10.0
+}
