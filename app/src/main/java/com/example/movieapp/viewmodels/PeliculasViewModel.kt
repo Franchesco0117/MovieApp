c7c5b8b6f323d23970654adecdc40d49829e6067
franciscocastromurillo18@gmail.com <franciscocastromurillo18@gmail.com>
2024-02-15 19:48:22 -0700

    ViewModel

---
+++ app/src/main/java/com/example/movieapp/viewmodels/PeliculasViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.movieapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieapp.core.Constantes
+import com.example.movieapp.models.PeliculaModel
+import com.example.movieapp.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class PeliculasViewModel : ViewModel() {
+    private var _listaPeliculas = MutableLiveData<List<PeliculaModel>>()
+    val listaPeliculas: LiveData<List<PeliculaModel>> = _listaPeliculas
+
+    fun obtenerCartelera() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = RetrofitClient.webService.obtenerCartelera(Constantes.API_Key)
+            withContext(Dispatchers.Main){
+                _listaPeliculas.value = response.body()!!.resultados.sortedByDescending { it.votoPromedio }
+            }
+        }
+    }
+
+    fun obtenerPopulares() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = RetrofitClient.webService.obtenerPopular(Constantes.API_Key)
+            withContext(Dispatchers.Main){
+                _listaPeliculas.value = response.body()!!.resultados.sortedByDescending { it.votoPromedio }
+            }
+        }
+    }
+}
